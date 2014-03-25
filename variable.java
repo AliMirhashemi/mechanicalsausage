cee3c46f4532a9f226f5a3cc2d9c7310bb124ead
AliMirhashemi <mirhashemiali@hotmail.com>
2014-03-25 11:42:29 -0700

    trying variables

---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+	public static void main (String[] args) {
+		int variable;
+		variable = 12345;
+
+		System.out.println(variable);
+
+	}
+
+}
---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+	public static void main (String[] args) {
+		int variable:
+		variable = 12345
+
+		System.out.println(variable);
+
+	}
+
+}
