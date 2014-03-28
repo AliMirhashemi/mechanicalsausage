a70e600d97b5e7f26320e82b58f25107a946af95
AliMirhashemi <mirhashemiali@hotmail.com>
2014-03-28 13:59:41 -0700

    using pi

---
+++ pi.java
@@ -0,0 +1,12 @@
+public class pi {
+	public static void main (String[] args) {
+		final double PI = 3.14159;
+
+		double radius = 20;
+
+		double area = radius * radius * PI;
+
+		System.out.println("the area for the circle of radius " + radius + " is " + area);
+
+	}
+}
