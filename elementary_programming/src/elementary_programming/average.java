92ae8cd5be97fd517a80e26f80680c5265c42138
AliMirhashemi <mirhashemiali@hotmail.com>
2014-04-01 23:15:07 -0700

    find the average of numbers

---
+++ elementary_programming/src/elementary_programming/average.java
@@ -0,0 +1,23 @@
+package elementary_programming;
+
+import java.util.Scanner;
+
+public class average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("enter three numbers: ");
+		double number1 = input.nextDouble();
+		double number2 = input.nextDouble();
+		double number3 = input.nextDouble();
+
+		double average = (number1 + number2 + number3) / 3;
+		 
+		System.out.println(" the average of your 3 numbers is " + average);
+	
+		}
+    }
+
